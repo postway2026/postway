e75d24821eda2fe963051971c7628cd8ca9ad492
postway2026 <postway2026@gmail.com>
2026-09-04 22:16:02 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+        cleanupOutdatedCaches: true,
+      },
       manifest: {
         name: "GM_0064 Do'kon Boshqaruvi",
         short_name: 'GM_0064',
